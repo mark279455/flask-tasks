91638984022c1eefbc9b013eeed78739fdf12bac
mark279455 <>
2023-06-05 14:40:01 +0000

    create and read complete for tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // sidenav initialization
-  let sidenav = document.querySelectorAll(".sidenav");
-  M.Sidenav.init(sidenav);
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // collapsible initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
