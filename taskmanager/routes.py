baaadcfe91f1148b55480a82e94fc39947feb37e
mark279455 <>
2023-06-05 12:16:57 +0000

    created functionality to add a new category

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -8,11 +8,16 @@ def home():
     return render_template("tasks.html")
 
 
-@app_route("/categories")
+@app.route("/categories")
 def categories():
     return render_template("categories.html")
 
 
-@app_route("/add_category", methods=["GET", "POST"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
